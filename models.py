8040a21bf7ba47f4e32f0e52afb47eda8528ecd8
Sehwan Yoo <krstyle03v@gmail.com>
2022-07-21 15:56:32 +0900

    .

--- models.py
+++ models.py
@@ -3,22 +3,9 @@ from tensorflow import keras
 from tensorflow.keras import layers 
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, Activation, MaxPooling2D, Dropout, Flatten
-
 import numpy as np
 
-
-import parameters
-
-# def create_class_weight(labels_dict, n_classes=10):
-#     total = np.sum(list(labels_dict.values()))
-#     keys = labels_dict.keys()
-#     class_weight = dict()
-    
-#     for idx, key in zip(range(n_classes), keys):
-#         score = total / (n_classes * train_dict[key])
-#         class_weight[idx] = score
-        
-#     return class_weight
+import main
 
 def get_dropout(input_tensor, p=0.3, mc=False):
     if mc: 
@@ -26,75 +13,80 @@ def get_dropout(input_tensor, p=0.3, mc=False):
         return layer(input_tensor, training=True)
     else:
         return Dropout(p, name='top_dropout')(input_tensor, training=False)
-
-
-def run_expriment(model_name, train_dataset, optimizer='adam', trainable=False, batch_size=32, mc=False, epochs=100): 
     
-    path = parameters.N_PATH
-    res = parameters.N_RES
-    classes = parameters.N_CLASSES
+
+def create_class_weight(train_dict):
+    total = np.sum(list(train_dict.values()))
+    class_weight = dict()
+    # for idx, key in label_to_index.items():
+    #     class_weight[key] = train_dict[idx] / total
+    class_weight[0] = train_dict[0] / total
+    class_weight[1] = train_dict[1] / total
+
+    return class_weight
+
+def create_model(model_name, optimizer='adam', num_classes=2, trainable=False, num_trainable=100, mc=False): 
+
+    # strategy = tf.distribute.MirroredStrategy()
+
+    # with strategy.scope():
     
     if model_name == 'efficient':
-        base_model = keras.applications.EfficientNetB4(include_top=False, input_shape=(res, res, 3),  weights = 'imagenet')
+        base_model = keras.applications.EfficientNetB4(include_top=False, input_shape=(main.num_res, main.num_res, 3),  weights = 'imagenet')
+        # base_model.trainable = trainable
+        
         base_model.trainable = trainable
         
-        inputs = keras.Input(shape=(res, res, 3))
+        if trainable:
+            for layer in base_model.layers[:num_trainable]:
+                layer.trainable = False
+        
+        inputs = keras.Input(shape=(main.num_res, main.num_res, 3))
         x = base_model(inputs)
         x = keras.layers.GlobalAveragePooling2D()(x) 
         x = get_dropout(x, mc)
-        x = keras.layers.Dense(classes, activation='softmax')(x)
+        # x = keras.layers.Dense(N_CLASSES, activation='softmax')(x)
+        x = keras.layers.Dense(1, activation='sigmoid')(x)
+        model = tf.keras.Model(inputs=inputs, outputs=x)
+        
+    elif model_name == 'mobilenet':
+        base_model = keras.applications.MobileNetV2(include_top=False, input_shape=(main.num_res, main.num_res, 3),  weights = 'imagenet')
+
+        base_model.trainable = trainable
+        if trainable:
+            for layer in base_model.layers[:num_trainable]:
+                layer.trainable = False
+
+        inputs = keras.Input(shape=(main.num_res, main.num_res, 3))
+        x = base_model(inputs)
+        x = keras.layers.GlobalAveragePooling2D()(x) 
+        x = get_dropout(x, mc)
+        # x = keras.layers.Dense(N_CLASSES, activation='softmax')(x)
+        x = keras.layers.Dense(1, activation='sigmoid')(x)
         model = tf.keras.Model(inputs=inputs, outputs=x)
         
     # VGG16 
     else:
-        base_model = keras.applications.VGG16(include_top=False, input_shape=(res, res, 3),  weights = 'imagenet')
+        base_model = keras.applications.VGG16(include_top=False, input_shape=(main.num_res, main.num_res, 3),  weights = 'imagenet')
         base_model.trainable = True
         
-        inputs = keras.Input(shape=(res, res, 3))
+        inputs = keras.Input(shape=(main.num_res, main.num_res, 3))
         x = base_model(inputs)
         x = keras.layers.Flatten(name = "avg_pool")(x) 
         x = keras.layers.Dense(512, activation='relu')(x)
         x = get_dropout(x, mc)
         x = keras.layers.Dense(256, activation='relu')(x)
-        x = keras.layers.Dense(classes, activation='softmax')(x)
+        x = keras.layers.Dense(num_classes, activation='softmax')(x)
         model = tf.keras.Model(inputs=inputs, outputs=x)
-        
 
-    sv = [tf.keras.callbacks.ModelCheckpoint(os.path.join(f'{path}/models/{model_name}_mc-{mc}_bs-{batch_size}_{time.strftime("%Y%m%d-%H%M%S")}.h5'), 
-                                             monitor='val_accuracy', 
-                                             verbose=0, 
-                                             save_best_only=True,
-                                             save_weights_only=True, 
-                                             mode='max', 
-                                             save_freq='epoch'), 
-          tf.keras.callbacks.EarlyStopping(monitor = 'val_accuracy', 
-                                           patience = 4, 
-                                           mode='auto',
-                                           min_delta = 0.01)
-          ]
-
-    
-    LR = 0.0001
-    steps_per_epoch = len(train_images) // batch_size
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(LR, steps_per_epoch*30, 0.1, True)
-    
-    # sgd = tf.keras.optimizers.SGD(0.01)
-    # moving_avg_sgd = tfa.optimizers.MovingAverage(sgd)
+    LR = 0.001
     if optimizer == 'adam':
-        optimizer = tf.keras.optimizers.Adam(lr_schedule)
+        optimizer = tf.keras.optimizers.Adam(LR)
     else:
-        optimizer = tf.keras.optimizers.SGD(lr_schedule)
-    
-    model.compile(loss=tfa.losses.SigmoidFocalCrossEntropy(), 
-                  optimizer = optimizer,
-                  metrics=['accuracy'])
-    
-    hist = model.fit(train_dataset,
-                    # validation_data = val_dataset,
-                    epochs = epochs,
-                    class_weight=class_weights, 
-                    validation_splits=0.3, 
-                    verbose = 1)
-    
-    
-    return model, hist
+        optimizer = tf.keras.optimizers.SGD(LR)
+        
+    model.compile(loss='binary_crossentropy', 
+                optimizer=optimizer,
+                metrics=['accuracy'])
+
+    return model
