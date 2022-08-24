fbf8caedc641a63adeccfe06800fb5878fdc9e7b
Sehwan Yoo <krstyle03v@gmail.com>
2022-08-24 16:01:53 +0900

    .

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,5 +1,6 @@
 import cv2 
 import tensorflow as tf
+import tensorflow_addons as tfa
 import os 
 from glob import glob 
 import random
@@ -8,57 +9,63 @@ import numpy as np
 import main
 import parameters
 
-# def create_all_dict():
-#     train_dict = {}
-#     # test_dict = {} 
-
-#     for i in range(6):
-#         files = os.listdir(os.path.join(main.dataset_path, f'H{i}'))
-        
-#         for f in files: 
-                
-#             imgs = glob(os.path.join(main.dataset_path, f'H{i}', f) + '/*.jpg')
-            
-#             key = 0 
-#             if f in main.infection_list:
-#                 key = 1
-                
-#             if key in train_dict:
-#                 train_dict[key] = train_dict[key] + len(imgs)
-#             else:
-#                 train_dict[key] = len(imgs)
-
-#     # add JeonNam unv 
-#     files = os.listdir(os.path.join(main.dataset_path, 'H9'))
-
-#     for f in files: 
-                
-#         imgs = glob(os.path.join(main.dataset_path, 'H9', f) + '/*.jpg')
-
-#         key = 0 
-#         if f in main.infection_list:
-#             key = 1
-
-#         if key in train_dict:
-#             train_dict[key] = train_dict[key]+ len(imgs)
-#         else:
-#             train_dict[key] = len(imgs)
-            
-#     return train_dict
+def aug1(img, lbl):
+    # shape augmentation
+    img = tf.image.random_crop(img, [main.num_res, main.num_res, 3])
+    img = tf.image.random_flip_left_right(img)
+    img = tf.image.flip_up_down(img)
+    
+    # color augmentation    
+    img = tf.image.random_brightness(img, 0.2) 
+    img = tf.image.random_contrast(img, 0.2, 0.5) 
+    img = tf.image.random_hue(img, 0.2)
+    img = tf.image.random_saturation(img, 0.6, 1.6)
+    
+    return img, lbl
 
 
-def create_train_list():
+def aug(img, label):
+    def flip(x):
+        x = tf.image.random_flip_left_right(x)
+        x = tf.image.random_flip_up_down(x)
+        return x
+    
+    def rotate(x):
+        x = tf.cond(tf.random.uniform(shape=[], minval=0.0, maxval=1.0, dtype=tf.float32) > 0.5,
+                   lambda: tfa.image.rotate(x,
+                                       tf.random.uniform(shape=[], minval=0.0, maxval=360.0, dtype=tf.float32),
+                                       interpolation='BILINEAR'),
+                   lambda: x)
+        return x
+    
+    def translation(x):
+        dx = tf.random.uniform(shape=[], minval=-10.0, maxval=10.0, dtype=tf.float32)
+        dy = tf.random.uniform(shape=[], minval=-10.0, maxval=10.0, dtype=tf.float32)
+        x = tf.cond(tf.random.uniform(shape=[], minval=0.0, maxval=1.0, dtype=tf.float32) > 0.5,
+                    lambda: tfa.image.transform(x,
+                                                [0, 0, dx, 0, 0, dy, 0, 0],
+                                                interpolation='BILINEAR'),
+                    lambda: x)
+        return x
+    
+    img = flip(img)
+    img = rotate(img)
+    img = translation(img)
+           
+    return img, label
+
+def create_train_list(part='head'):
     train_images = [] 
     test_images = []
 
     for i in range(6):
         # for key in train_dict.keys():
         
-        img = glob(parameters.dataset_path + f'/H{str(i)}/*/*.jpg')
+        img = glob(parameters.dataset_path + f'/H{str(i)}/*/{part}/*.jpg')
         train_images.extend(img) 
 
     # add JeonNam unv
-    img = glob(parameters.dataset_path + '/H9/*/*.jpg')
+    img = glob(parameters.dataset_path + f'/H9/*/{part}/*.jpg')
     train_images.extend(img) 
             
     random.shuffle(train_images)
@@ -66,12 +73,6 @@ def create_train_list():
     train_labels = [] 
     for img in train_images: 
         lbl = img.split('/')[-2].lower().replace(' ', '')
-        
-        # if lbl in main.name_dict:
-        #     lbl = main.name_dict[lbl]
-        
-        # if lbl not in main.class_list:
-        #     print(f'WARNING! NO FOUND CALASS : {lbl}')
         
         if lbl in parameters.name_dict:
             lbl = parameters.name_dict[lbl][0]
@@ -88,222 +89,80 @@ def create_train_list():
     train_images = np.reshape(train_images, [-1, 1])
     train_labels = np.reshape(train_labels, [-1, 1])
     
-    
     return train_images, train_labels
 
 
-def create_dataset(images, labels, d_type='train', aug=False):
+def create_test_list(part='head'):
+    test_images = []
+
+    for i in range(6):
+        # for key in train_dict.keys():
+        
+        img = glob(parameters.dataset_path + f'/H{str(i)}/*/{part}/*.jpg')
+        test_images.extend(img) 
+
+    # add JeonNam unv
+    img = glob(parameters.dataset_path + f'/H9/*/{part}/*.jpg')
+    test_images.extend(img) 
+            
+    random.shuffle(test_images)
+
+    test_labels = [] 
+    for img in test_images: 
+        lbl = img.split('/')[-2].lower().replace(' ', '')
+        
+        if lbl in parameters.name_dict:
+            lbl = parameters.name_dict[lbl][0]
+        
+        # lbl
+        if lbl in parameters.infection_list:
+            lbl = 1 
+        else:
+            lbl = 0 
+
+        test_labels.append(lbl) 
+
+
+    test_images = np.reshape(test_images, [-1, 1])
+    test_labels = np.reshape(test_labels, [-1, 1])
+    
+    return test_images, test_labels
+
+
+def create_dataset(images, labels, d_type='train'):
     
     if d_type == 'test':
-        return tf.data.Dataset.from_generator(test_skin_data, 
-                                              output_types=(tf.float64, tf.float32), 
-                                              output_shapes=(tf.TensorShape([parameters.num_res, parameters.num_res, 3]), tf.TensorShape([1])),
-                                              args=[images, labels])
+        return tf.data.Dataset.from_generator(
+            test_skin_data, 
+            output_types=(tf.float64, tf.float32), 
+            output_shapes=(tf.TensorShape([parameters.num_res, parameters.num_res, 3]), tf.TensorShape([1])),
+            args=[images, labels])
         
     else:
-        return tf.data.Dataset.from_generator(train_skin_data, 
-                                              output_types=(tf.float64, tf.float32), 
-                                              output_shapes=(tf.TensorShape([parameters.num_res, parameters.num_res, 3]), tf.TensorShape([1])),
-                                              args=[images, labels, aug])
-    # return train_dct
-    
-
-# def cutmix(images, labels):
-#     # imgs = []; labs = []
-#     # for i in range(N_BATCH):
-#     APPLY = tf.cast(tf.random.uniform(()) >= 0.5, tf.int32)
-#     idx = tf.random.uniform((), 0, len(train_images), tf.int32)
-    
-#     # random_img = 0 
-#     # random_lbl = 0 
-    
-#     random_img = cv2.imread(train_images[idx], cv2.COLOR_BGR2YCR_CB)
-#     random_img = cv2.resize(random_img, (parameters.N_RES, parameters.N_RES))
-#     random_img = cv2.normalize(random_img, None, 0, 255, cv2.NORM_MINMAX)
-    
-#     random_idx = train_images[idx].split('\\')[2]
-    
-#     random_key = 0 
-#     if random_idx in parameters.infection:
-#         random_key = 1
-        
-#     random_lbl = tf.keras.utils.to_categorical(random_key, parameters.N_CLASSES)
-
-#     W = parameters.N_RES
-#     H = parameters.N_RES
-#     lam = tf.random.uniform(())
-#     cut_ratio = tf.math.sqrt(1.-lam)
-#     cut_w = tf.cast(W * cut_ratio, tf.int32) * APPLY
-#     cut_h = tf.cast(H * cut_ratio, tf.int32) * APPLY
-
-#     cx = tf.random.uniform((), int(W/8), int(7/8*W), tf.int32)
-#     cy = tf.random.uniform((), int(H/8), int(7/8*H), tf.int32)
-
-#     xmin = tf.clip_by_value(cx - cut_w//2, 0, W)
-#     ymin = tf.clip_by_value(cy - cut_h//2, 0, H)
-#     xmax = tf.clip_by_value(cx + cut_w//2, 0, W)
-#     ymax = tf.clip_by_value(cy + cut_h//2, 0, H)
-
-#     mid_left = images[ymin:ymax, :xmin, :]
-#     # mid_mid = images[idx, ymin:ymax, xmin:xmax, :]
-#     mid_mid = random_img[ymin:ymax, xmin:xmax, :]
-#     mid_right = images[ymin:ymax, xmax:, :]
-#     middle = tf.concat([mid_left, mid_mid, mid_right], axis=1)
-#     top = images[:ymin, :, :]
-#     bottom = images[ymax:, :, :]
-#     new_img = tf.concat([top, middle, bottom], axis=0)
-#     # imgs.append(new_img)
-
-#     cut_w_mod = xmax - xmin
-#     cut_h_mod = ymax - ymin
-#     alpha = tf.cast((cut_w_mod*cut_h_mod)/(W*H), tf.float32)
-#     # label1 = labels[i]
-#     label1 = labels
-#     # label2 = labels[idx]
-#     label2 = random_lbl
-#     new_label = ((1-alpha)*label1 + alpha*label2)
-#     # labs.append(new_label)
-        
-#     # new_imgs = tf.reshape(tf.stack(imgs), [-1, N_RES, N_RES, 3])
-#     # new_labs = tf.reshape(tf.stack(labs), [-1, N_CLASSES])
-
-#     return new_img, new_label
-
-# def cutmix(images, labels):
-#     # imgs = []; labs = []
-#     # for i in range(N_BATCH):
-#     APPLY = tf.cast(tf.random.uniform(()) >= 0.5, tf.int32)
-#     idx = tf.random.uniform((), 0, len(train_images), tf.int32)
-    
-#     # random_img = 0 
-#     # random_lbl = 0 
-    
-#     random_img = cv2.imread(train_images[idx], cv2.COLOR_BGR2YCR_CB)
-#     random_img = cv2.resize(random_img, (N_BEF_RES, N_BEF_RES))
-#     random_img = cv2.normalize(random_img, None, 0, 255, cv2.NORM_MINMAX)
-    
-#     random_idx = train_images[idx].split('/')[-2]
-    
-#     random_key = 0 
-#     if random_idx in infection_list:
-#         random_key = 1
-        
-#     random_lbl = tf.keras.utils.to_categorical(random_key, N_CLASSES)
-
-#     W = N_RES
-#     H = N_RES
-#     lam = tf.random.uniform(())
-#     cut_ratio = tf.math.sqrt(1.-lam)
-#     cut_w = tf.cast(W * cut_ratio, tf.int32) * APPLY
-#     cut_h = tf.cast(H * cut_ratio, tf.int32) * APPLY
-
-#     cx = tf.random.uniform((), int(W/8), int(7/8*W), tf.int32)
-#     cy = tf.random.uniform((), int(H/8), int(7/8*H), tf.int32)
-
-#     xmin = tf.clip_by_value(cx - cut_w//2, 0, W)
-#     ymin = tf.clip_by_value(cy - cut_h//2, 0, H)
-#     xmax = tf.clip_by_value(cx + cut_w//2, 0, W)
-#     ymax = tf.clip_by_value(cy + cut_h//2, 0, H)
-
-#     mid_left = images[ymin:ymax, :xmin, :]
-#     # mid_mid = images[idx, ymin:ymax, xmin:xmax, :]
-#     mid_mid = random_img[ymin:ymax, xmin:xmax, :]
-#     mid_right = images[ymin:ymax, xmax:, :]
-#     middle = tf.concat([mid_left, mid_mid, mid_right], axis=1)
-#     top = images[:ymin, :, :]
-#     bottom = images[ymax:, :, :]
-#     new_img = tf.concat([top, middle, bottom], axis=0)
-#     # imgs.append(new_img)
-
-#     cut_w_mod = xmax - xmin
-#     cut_h_mod = ymax - ymin
-#     alpha = tf.cast((cut_w_mod*cut_h_mod)/(W*H), tf.float32)
-#     # label1 = labels[i]
-#     label1 = labels
-#     # label2 = labels[idx]
-#     label2 = random_lbl
-#     new_label = ((1-alpha)*label1 + alpha*label2)
-#     # labs.append(new_label)
-        
-#     # new_imgs = tf.reshape(tf.stack(imgs), [-1, N_RES, N_RES, 3])
-#     # new_labs = tf.reshape(tf.stack(labs), [-1, N_CLASSES])
-
-#     return new_img, new_label
+        return tf.data.Dataset.from_generator(
+            train_skin_data, 
+            output_types=(tf.float64, tf.float32), 
+            output_shapes=(tf.TensorShape([parameters.num_res, parameters.num_res, 3]), tf.TensorShape([1])),
+            args=[images, labels])
 
 # def train_skin_data(files):
 def train_skin_data(images, labels, aug):
-    
-    # for file in files:
     for img, lbl in zip(images, labels):
     
         img = img[0].decode('utf-8')
         img = cv2.imread(img, cv2.COLOR_BGR2RGB)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (parameters.num_res, parameters.num_res))
-        # img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
-
-        # print(f'=================>{f}')
-        # idx = f.split('\\')[2]
-        # idx = f.split('/')[-2]
-        
-        # key = 0 
-        # if idx in infection:
-        #     key = 1 
-        # lbl = tf.keras.utils.to_categorical(key, N_CLASSES)
 
         yield (img, lbl)    
 
-        # if aug:
-        #     pass
-        
-        # if lower than base num, should apply data augmentation
-        # if base_num <= int(train_dict[idx]):
-        # if key == 0:
-            
-        #     # saturated
-        #     saturated_img = tf.image.adjust_saturation(img, 3)
-        #     yield (saturated_img, lbl)
-            
-        #     # flip 
-        #     random_flip_img = tf.image.random_flip_left_right(img)
-        #     yield (random_flip_img, lbl) 
-            
-        #     # Btight 
-        #     random_bright_img = tf.image.random_brightness(img, max_delta=0.5)
-        #     yield (random_bright_img, lbl) 
-    
-        #     # rotation 90 
-        #     rotated_img = tf.image.rot90(img)        
-        #     yield (rotated_img, lbl) 
-            
-        #     # rotation 180
-        #     rotated_img = tf.image.rot90(img, k=2)        
-        #     yield (rotated_img, lbl) 
-            
-        #     # rotation 270 
-        #     rotated_img = tf.image.rot90(img, k=3)        
-        #     yield (rotated_img, lbl) 
-            
-        #     # curmix 
-        #     cutmixed_img, cutmixed_lbl = cutmix(img, lbl)
-        #     yield (cutmixed_img, cutmixed_lbl)
-            
             
 def test_skin_data(images, labels,):
-    
     for img, lbl in zip(images, labels):
     
-        img = img.decode('utf-8')
-        
+        img = img[0].decode('utf-8')
         img = cv2.imread(img, cv2.COLOR_BGR2RGB)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (parameters.num_res, parameters.num_res))
-        # img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
         
-        # key = 0 
-        # if idx in infection_list:
-        #     key = 1 
-            
-        # lbl = tf.keras.utils.to_categorical(key, N_CLASSES)
-
         yield (img, lbl)    
--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import math
 
 import time
 import pandas as pd
+import argparse
 
 
 import models 
@@ -39,75 +40,88 @@ silence_tensorflow()
 AUTOTUNE = tf.data.AUTOTUNE
 
             
-# train_dict, test_dict = dataset.create_dict()
-# N_CLASSES = len(train_dict)
-
-            # Open a strategy scope.
-            # def create_model(model_name, optimizer='adam', trainable=False, mc=False
+if __name__ == '__main__':
             
-train_images, train_labels = dataset_generator.create_train_list() 
-            
-for skf_num in [5, 10]:
-        skf = StratifiedKFold(n_splits=skf_num)
+    parser = argparse.ArgumentParser()
         
-        kfold = 0 
-        for train_idx, valid_idx in skf.split(train_images, train_labels):
+    parser.add_argument('--model_name', required=False, default='efficient')
+    parser.add_argument('--gpus', required=False, default=2)
+    # parser.add_argument('--min_num', required=False, default=min_num)
+    # parser.add_argument('--max_num', required=False, default=max_num)
+    parser.add_argument('--part', required=False, default='head')
+    parser.add_argument('--epochs', required=False, default=500)
+    
+    args = parser.parse_args()
+    
+    gpus = []
+    for gpu in range(args.gpus):
+        gpus.append(f'/GPU:{gpu}')
+    
+    train_images, train_labels = dataset_generator.create_train_list() 
+                
+    for skf_num in [5, 10]:
+            skf = StratifiedKFold(n_splits=skf_num)
             
-            strategy = tf.distribute.MirroredStrategy(devices=['/device:GPU:0', '/device:GPU:1', '/device:GPU:2'],
-                                                      cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
-            with strategy.scope():
+            kfold = 0 
+            for train_idx, valid_idx in skf.split(train_images, train_labels):
                 
-                train_dataset = dataset_generator.create_dataset(train_images[train_idx], train_labels[train_idx], aug=False) 
-                valid_dataset = dataset_generator.create_dataset(train_images[valid_idx], train_labels[valid_idx]) 
                 
-
-                train_dataset = train_dataset.batch(parameters.num_batch, drop_remainder=True).prefetch(AUTOTUNE)
-                valid_dataset = valid_dataset.batch(parameters.num_batch, drop_remainder=True).prefetch(AUTOTUNE)
+                mirrored_strategy = tf.distribute.MirroredStrategy(devices=gpus)
                 
-                model = models.create_model('resnet', 
-                                            optimizer='sgd', 
-                                            num_classes=parameters.num_classes, 
-                                            trainable=True, 
-                                            num_trainable=-2,
-                                            batch_size=parameters.num_batch,
-                                            train_length=len(train_images[train_idx]))
+                with mirrored_strategy.scope():
+                    
+                    train_dataset = dataset_generator.create_dataset(train_images[train_idx], train_labels[train_idx]) 
+                    valid_dataset = dataset_generator.create_dataset(train_images[valid_idx], train_labels[valid_idx]) 
+                    
+                    train_dataset = train_dataset.map(dataset_generator.aug1, num_parallel_calls=AUTOTUNE).batch(parameters.num_batch, drop_remainder=True).prefetch(AUTOTUNE)
+                    valid_dataset = valid_dataset.map(dataset_generator.aug1, num_parallel_calls=AUTOTUNE).batch(parameters.num_batch, drop_remainder=True).prefetch(AUTOTUNE)
+                    
+                    model = models.create_model(args.model_name, 
+                                                optimizer='sgd', 
+                                                num_classes=parameters.num_classes, 
+                                                trainable=True, 
+                                                num_trainable=-2,
+                                                batch_size=parameters.num_batch,
+                                                train_length=len(train_images[train_idx]))
 
-                
+                    
 
-                # model, hist = run_expriment('efficient', train_dataset, valid_dataset, class_weights=None, optimizer='sgd', trainable=False, batch_size=N_BATCH, mc=False, epochs=50)
-                
-                filepath = os.path.join(f'../../models/child_classification_infection/check_point_efficient_{time.strftime("%Y%m%d-%H%M%S")}.h5')
-                
+                    # model, hist = run_expriment('efficient', train_dataset, valid_dataset, class_weights=None, optimizer='sgd', trainable=False, batch_size=N_BATCH, mc=False, epochs=50)
+                    
+                    filepath = os.path.join(f'../../models/child_classification_infection/check_point_efficient_{time.strftime("%Y%m%d-%H%M%S")}.h5')
+                    
 
-                checkpoints = [tf.keras.callbacks.ModelCheckpoint(filepath, 
-                                                                  monitor='val_accuracy', 
-                                                                  verbose=0, 
-                                                                  save_best_only=True,
-                                                                  save_weights_only=False, 
-                                                                  mode='max', 
-                                                                  freq='epoch'), 
-                               tf.keras.callbacks.ModelCheckpoint(filepath, 
-                                                                  monitor='val_loss', 
-                                                                  verbose=0, 
-                                                                  save_best_only=True,
-                                                                  save_weights_only=False, 
-                                                                  mode='min', 
-                                                                  freq='epoch')]
-                
-                hist = model.fit(train_dataset, 
-                                validation_data=valid_dataset,
-                                epochs = 100,
-                                verbose = 1,
-                                callbacks=[checkpoints])  
+                    checkpoints1 = [tf.keras.callbacks.ModelCheckpoint(filepath, 
+                                                                    monitor='val_accuracy', 
+                                                                    verbose=0, 
+                                                                    save_best_only=True,
+                                                                    save_weights_only=False, 
+                                                                    mode='max', 
+                                                                    freq='epoch')]
+                    
+                     
+                    checkpoints2 = [tf.keras.callbacks.ModelCheckpoint(filepath, 
+                                                                    monitor='val_loss', 
+                                                                    verbose=0, 
+                                                                    save_best_only=True,
+                                                                    save_weights_only=False, 
+                                                                    mode='min', 
+                                                                    freq='epoch')]
+                    
+                    hist = model.fit(train_dataset, 
+                                    validation_data=valid_dataset,
+                                    epochs = args.epochs,
+                                    # verbose = 1,
+                                    callbacks=[checkpoints1, checkpoints2])  
 
 
-            # evaluation
-            model.save(f'../../models/child_classification_infection/{time.strftime("%Y%m%d-%H%M%S")}_efficientb4_infection_kfold_{skf_num}_{kfold}.h5')
+                # evaluation
+                model.save(f'../../models/child_classification_infection/{time.strftime("%Y%m%d-%H%M%S")}_{args.model_name}_infection_kfold_{skf_num}_{kfold}.h5')
 
-            # import pandas as pd
-            hist_df = pd.DataFrame(hist.history)
-            with open(f'../../models/child_classification_infection/{time.strftime("%Y%m%d-%H%M%S")}_efficientb4_infection_kfold_{skf_num}_{kfold}.csv', mode='w') as f:
-                hist_df.to_csv(f)
+                # import pandas as pd
+                hist_df = pd.DataFrame(hist.history)
+                with open(f'../../models/child_classification_infection/{time.strftime("%Y%m%d-%H%M%S")}_{args.model_name}_infection_kfold_{skf_num}_{kfold}.csv', mode='w') as f:
+                    hist_df.to_csv(f)
 
-            kfold += 1
+                kfold += 1
 
