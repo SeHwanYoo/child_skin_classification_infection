763e9813864aa4ad14c18e3f4d4eb670421da87f
Sehwan Yoo <krstyle03v@gmail.com>
2022-08-24 16:42:31 +0900

    .

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
             for train_idx, valid_idx in skf.split(train_images, train_labels):
                 
                 
-                mirrored_strategy = tf.distribute.MirroredStrategy(devices=gpus)
+                mirrored_strategy = tf.distribute.MirroredStrategy(devices=gpus, cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
                 
                 with mirrored_strategy.scope():
                     
